961b9e5319b099bb9e426fa441a03bf9de7f17bf
Carla Polanco <carla.polanco@usach.cl>
2022-07-11 00:48:36 -0400

    [Carla Polanco] - CRUD Voluntario

--- backend-ayni/src/main/java/cl/tbd/backendayni/repositories/VoluntarioRepository.java
+++ backend-ayni/src/main/java/cl/tbd/backendayni/repositories/VoluntarioRepository.java
@@ -3,10 +3,12 @@ package cl.tbd.backendayni.repositories;
 import cl.tbd.backendayni.models.Voluntario;
 import java.util.List;
 
+import org.springframework.data.mongodb.repository.MongoRepository;
+
 /**
  * Interface para VoluntarioRepository
  */
-public interface VoluntarioRepository {
+public interface VoluntarioRepository extends MongoRepository<Voluntario, String>{
     
     public int countVoluntarios();
     public int newId();
@@ -16,5 +18,14 @@ public interface VoluntarioRepository {
     public Voluntario createVoluntario(Voluntario voluntario);
     public void deleteVoluntarioById(long id);
     public void updateVoluntario(Voluntario voluntario);
+
+    
+    //CRUD de Mongodb
+    public Voluntario findByNombre(String nombre);
+	public List<Voluntario> find();
+	public Voluntario insertOneVoluntario(Voluntario voluntario);
+	public Voluntario updVoluntario(Voluntario voluntario); 
+	public void deleteVoluntario(String id);
+
 }
 
