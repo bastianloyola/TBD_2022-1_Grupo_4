19d4bcabe259bfd3ebe5f59f5f9f7ce737817c0f
Bastian Onetto <Bastian.onetto@usach.cl>
2022-07-11 22:48:19 -0400

    [Bastian Onetto] - Arreglo Datos en Tabla Voluntario y Tarea

--- backend-ayni/src/main/java/cl/tbd/backendayni/BackendAyniApplication.java
+++ backend-ayni/src/main/java/cl/tbd/backendayni/BackendAyniApplication.java
@@ -30,22 +30,22 @@ public class BackendAyniApplication implements CommandLineRunner {
 
 		repositoryTarea.deleteAll();
 		// get today date as string
-		repositoryTarea.save(new Tarea("Tarea 1", "Descripcion 1", "Estado 1", null, "Voluntario 1", "Voluntario 2",
-				"Voluntario 3"));
-		repositoryTarea.save(new Tarea("Tarea 2", "Descripcion 2", "Estado 2", null, "Voluntario 1", "Voluntario 2",
-				"Voluntario 3"));
-		repositoryTarea.save(new Tarea("Tarea 3", "Descripcion 3", "Estado 3", null, "Voluntario 1", "Voluntario 2",
-				"Voluntario 3"));
-		repositoryTarea.save(new Tarea("Tarea 4", "Descripcion 4", "Estado 4", null, "Voluntario 1", "Voluntario 2",
-				"Voluntario 3"));
-		repositoryTarea.save(new Tarea("Tarea 5", "Descripcion 5", "Estado 5", null, "Voluntario 1", "Voluntario 2",
-				"Voluntario 3"));
+		repositoryTarea.save(new Tarea("1", "Recoger basura", "Recoger escombros, botarlos en un sitio seguro", "07/09/2022", "Pala, Rastrillo", "-33.389.804",
+				"-70.655.240"));
+		repositoryTarea.save(new Tarea("2", "Recoger ropa", "Recoger ropa para los necesitados", "28/06/2022", "Auto, Bolsas tamaño jumbo", "-33.450.758",
+				"-70.679.715"));
+		repositoryTarea.save(new Tarea("3", "Limpiar zona", "Limpiar zona afectada por la emergencia", "08/11/2022", "Pala, Chuzo", "-32.878.800",
+				"-71.455.213"));
+		repositoryTarea.save(new Tarea("4", "Alertar a personas", "Anunciar a las personas sobre la emergencia", "14/02/2022", "Megafono, Parlante", "-33.444.194",
+				"-70.653.674"));
+		repositoryTarea.save(new Tarea("5", "Poner centros de acopio", "Colocar centros de acopio para las personas necesitadas", "01/10/2022", "Mesas, Toldos", "-33.458.078",
+				"-70.640.655"));
 
 		repositoryVoluntario.deleteAll();
-		repositoryVoluntario.save(new Voluntario("correo1", "usuario1", "nombre1", "password1", "atributos1"));
-		repositoryVoluntario.save(new Voluntario("correo2", "usuario2", "nombre2", "password2", "atributos2"));
-		repositoryVoluntario.save(new Voluntario("correo3", "usuario3", "nombre3", "password3", "atributos3"));
-		repositoryVoluntario.save(new Voluntario("correo4", "usuario4", "nombre4", "password4", "atributos4"));
+		repositoryVoluntario.save(new Voluntario("michaelRThomas@gmail.com", "Hatter", "Michael Thomas", "baebe2jei1Oh", "Responsable, Esforzado"));
+		repositoryVoluntario.save(new Voluntario("kyleFDulac@gmail.com", "Hinceeng49", "Kyle Dulac", "jeeFi7ae", "Eficiente, Acertivo"));
+		repositoryVoluntario.save(new Voluntario("susanBTaylor@gmail.com", "Beeked", "Susan Taylor", "oahee9AeNg5", "Flojo, Certero"));
+		repositoryVoluntario.save(new Voluntario("maryDWisniewski@gmail.com", "Dialt1937", "Mary DWisneiwski", "Aeveip5xu2", "Honesto, Insoportable"));
 
 	}
 
